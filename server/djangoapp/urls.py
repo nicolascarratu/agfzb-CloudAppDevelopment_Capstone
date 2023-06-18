50e09030c2a6c6a924cff2f9d46df38d0b3c638c
nicolascarratu <carratutommasino@gmail.com>
2023-06-18 14:50:45 -0400

    user management

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-
+  path(route='', view=views.get_dealerships, name='index'),
     # path for about view
     path(route='about', view=views.get_about, name='about'),
 
@@ -17,12 +17,14 @@ urlpatterns = [
     path(route='contact', view=views.get_contact, name='contact'),    
 
     # path for registration
-
+    path('registration/', view=views.registration_request, name='registration'),
     # path for login
+    path('login/', views.login_request, name='login'),
 
     # path for logout
+    path('logout/', views.logout_request, name='logout'),
 
-    path(route='', view=views.get_dealerships, name='index'),
+  
 
     # path for dealer reviews view
 
